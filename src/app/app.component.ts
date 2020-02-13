4371f3430a6f00b445d30ff5ab3a20ec44c1858f
Andres Guerrero <andresggcf@gmail.com>
2020-02-13 11:45:12 -0500

    segundo intento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+/*Importamos la base de datos de las cuentas*/
+//import data  from './data/data.json'; 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pruebas-BDO';
+
+  ngOnInit(): void {
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule}  from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDividerModule } from '@angular/material/divider';
+import { OtrosCardComponent } from './otros-card/otros-card.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    OtrosCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    MatButtonToggleModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
